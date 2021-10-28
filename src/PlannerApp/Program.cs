94181999e1d912ecc209d3c9429cf341fe32f12f
Houndonougbo G. Vincent <houndvince@gmail.com>
2021-10-28 23:13:04 +0000

    setup authorization message handler

--- src/PlannerApp/Program.cs
+++ src/PlannerApp/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using MudBlazor.Services;
@@ -14,9 +15,19 @@ namespace PlannerApp
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddHttpClient("PlannerApp.Api", client =>
+            {
+                client.BaseAddress = new Uri("https://plannerapp-api.azurewebsites.net");
+            }).AddHttpMessageHandler<AuthorizationMessageHandler>();
+
+            builder.Services.AddTransient<AuthorizationMessageHandler>();
+
+            builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>()
+                            .CreateClient("PlannerApp.Api"));
 
             builder.Services.AddMudServices();
+
+            builder.Services.AddBlazoredLocalStorage();
 
             await builder.Build().RunAsync();
         }
